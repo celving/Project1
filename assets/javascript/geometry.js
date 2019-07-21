b9c92900c83d77c8339e75f91bafed049dde6634
DaRican <jcastillo8150@gmail.com>
2019-07-20 21:00:20 -0500

    moved all js code to geometry, made changes to title

--- assets/javascript/geometry.js
+++ assets/javascript/geometry.js
@@ -2,6 +2,8 @@ function initMap() {
 
     var map;
 
+
+
     // Funtions made to covert meters to miles, and miles to meters 
     function convertToMiles(meters) {
         var miles = meters * 0.000621371;
@@ -17,17 +19,19 @@ function initMap() {
     };
 
     var firstAddress;
+    var secondAddress;
+    var addressArray = [];
 
     $('#addressSubmit').on('click', function () {
         event.preventDefault();
         firstAddress = $('#userInput').val().trim();
-        logFirstAddress();
-
-
+        secondAddress = $('#friendInput').val().trim();
+        addressArray.push(firstAddress, secondAddress);
+        $('#userInput').val("");
+        $('#friendInput').val("");
+        logAddresses();
 
     });
-    console.log(firstAddress);
-    console.log(google);
 
 
     map = new google.maps.Map(document.getElementById('googleMap'), {
@@ -35,29 +39,64 @@ function initMap() {
         zoom: 10
     });
 
-    console.log(map);
+
     var geocoder = new google.maps.Geocoder();
 
-    function logFirstAddress() {
+    var lat1;
+    var lng1;
+    var lat2;
+    var lng2;
+    var centerMarker = [];
 
-        var addressConverter = geocoder.geocode({ 'address': firstAddress }, function (results, status) {
+    function logAddresses() {
+
+
+
+        var addressConverterOne = geocoder.geocode({ 'address': firstAddress }, function (results, status) {
 
             console.log(status);
             var marker;
-            var lat = results[0].geometry.viewport.na.j;
-            var lng = results[0].geometry.viewport.ga.j;
-            var geoAddress = { lat: lat, lng: lng };
-            marker = new google.maps.Marker({ position: geoAddress, map: map });
+            lat1 = results[0].geometry.viewport.na.j;
+            lng1 = results[0].geometry.viewport.ga.j;
+            centerMarker.push(lat1, lng1);
+            var geoAddress = { lat: lat1, lng: lng1 };
+            marker1 = new google.maps.Marker({ position: geoAddress, map: map });
 
             $('#userInput').val("");
             $('#userInput').empty();
 
+        });
 
+        var addressConverterTwo = geocoder.geocode({ 'address': secondAddress }, function (results, status) {
 
+            console.log("GEOCODER STATUS: " + status);
+            var marker;
+            lat2 = results[0].geometry.viewport.na.j;
+            lng2 = results[0].geometry.viewport.ga.j;
+            centerMarker.push(lat2, lng2);
+            var geoAddress = { lat: lat2, lng: lng2 };
+            marker2 = new google.maps.Marker({ position: geoAddress, map: map });
+
+            $('#userInput').val("");
+            $('#userInput').empty();
+
+            findCenter();
         });
 
 
 
+
+        addressArray = [];
+        console.log(centerMarker);
+
+        function findCenter() {
+            var _kCord = new google.maps.LatLng(centerMarker[0], centerMarker[1]);
+            var _pCord = new google.maps.LatLng(centerMarker[2], centerMarker[3]);
+
+            var centerPosition = google.maps.geometry.spherical.interpolate(_kCord, _pCord, 0.5);
+
+            var halfwayMarker = new google.maps.Marker({ position: centerPosition, map: map });
+        };
 
 
 
@@ -68,6 +107,10 @@ function initMap() {
 
 
     };
+
+
+
+
 
 
 
