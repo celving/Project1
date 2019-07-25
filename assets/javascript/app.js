57a4fbef824ca0a3b4c0ded56fbe2bb3b804dc56
Colin <viewtifuljho@hotmail.com>
2019-07-25 18:51:06 -0500

    added autofill and radius/category functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -234,7 +234,33 @@ $(document).ready(function () {
         };
 
 
+        function initAutocompleteuser() {
+            // Create the autocomplete object, restricting the search predictions to
+            // geographical location types.
+            autocomplete = new google.maps.places.Autocomplete(
+                document.getElementById('userInput'), {types: ['geocode']});
+          
+            // Avoid paying for data that you don't need by restricting the set of
+            // place fields that are returned to just the address components.
+            autocomplete.setFields(['address_component']);
+          
+                    }
 
+                    function initAutocompletefriend() {
+                        // Create the autocomplete object, restricting the search predictions to
+                        // geographical location types.
+                        autocomplete = new google.maps.places.Autocomplete(
+                            document.getElementById('friendInput'), {types: ['geocode']});
+                      
+                        // Avoid paying for data that you don't need by restricting the set of
+                        // place fields that are returned to just the address components.
+                        autocomplete.setFields(['address_component']);
+                      
+                                }
+            
+
+          initAutocompleteuser();
+          initAutocompletefriend();
 
 
 
